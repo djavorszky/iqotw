ab4850ff446282c7ee477612664f50eb6a76f85b
Daniel Javorszky <daniel.javorszky@gmail.com>
2022-02-25 22:16:27 +0100

    Add less performant version to feb20

--- benches/benchmark.rs
+++ benches/benchmark.rs
@@ -28,8 +28,29 @@ fn feb20_benchmark(c: &mut Criterion) {
             169, 140, 153,
         ],
     ] {
-        group.bench_function(format!("Length: {}", input.len()), |b| {
+        group.bench_function(format!("length-{}", input.len()), |b| {
             b.iter(|| longest_sub_seq(black_box(&input)))
+        });
+    }
+
+    group.finish();
+
+    let mut group = c.benchmark_group("feb_20/longest_subseq_map");
+
+    for input in vec![
+        vec![1, 9, 87, 3, 10, 4, 20, 2, 45],
+        vec![36, 41, 56, 35, 91, 33, 34, 92, 43, 37, 42],
+        vec![
+            117, 148, 107, 70, 177, 155, 49, 191, 164, 190, 141, 128, 7, 192, 173, 197, 41, 131,
+            143, 182, 95, 162, 66, 62, 59, 97, 157, 77, 68, 123, 147, 60, 34, 109, 84, 113, 44, 57,
+            28, 187, 5, 165, 171, 100, 22, 43, 181, 176, 11, 151, 36, 200, 12, 54, 172, 69, 194,
+            91, 61, 6, 46, 81, 118, 183, 79, 144, 52, 87, 88, 58, 111, 17, 189, 185, 76, 40, 50,
+            23, 37, 130, 72, 78, 150, 71, 2, 96, 20, 80, 112, 124, 18, 89, 127, 110, 163, 142, 39,
+            169, 140, 153,
+        ],
+    ] {
+        group.bench_function(format!("length-{}", input.len()), |b| {
+            b.iter(|| longest_sub_seq_map(black_box(&input)))
         });
     }
 
--- src/y22/feb20.rs
+++ src/y22/feb20.rs
@@ -2,6 +2,8 @@
 // such that elements in the sub-sequence are consecutive integers, the
 // consecutive numbers can be in any order.
 
+use std::collections::HashSet;
+
 pub fn longest_sub_seq(input: &[i32]) -> usize {
     let mut clone: Vec<i32> = input.iter().cloned().collect();
     clone.sort();
@@ -22,6 +24,37 @@ pub fn longest_sub_seq(input: &[i32]) -> usize {
     counts.into_iter().max().unwrap()
 }
 
+pub fn longest_sub_seq_map(input: &[i32]) -> usize {
+    let orig: HashSet<i32> = input.iter().cloned().collect();
+    let mut seen = HashSet::with_capacity(input.len());
+    let mut highest_count = 1;
+
+    for n in input {
+        if seen.contains(n) {
+            continue;
+        }
+        let mut count = 1;
+
+        let mut next = n + 1;
+        while orig.contains(&next) {
+            seen.insert(next);
+            next += 1;
+            count += 1;
+        }
+
+        let mut prev = n - 1;
+        while orig.contains(&prev) {
+            seen.insert(prev);
+            prev += 1;
+            count += 1;
+        }
+
+        highest_count = highest_count.max(count);
+    }
+
+    highest_count
+}
+
 #[cfg(test)]
 mod tests {
 
@@ -32,6 +65,15 @@ mod tests {
         assert_eq!(longest_sub_seq(&[1, 9, 87, 3, 10, 4, 20, 2, 45]), 4);
         assert_eq!(
             longest_sub_seq(&[36, 41, 56, 35, 91, 33, 34, 92, 43, 37, 42]),
+            5
+        );
+    }
+
+    #[test]
+    fn test_longest_sub_seq_map() {
+        assert_eq!(longest_sub_seq_map(&[1, 9, 87, 3, 10, 4, 20, 2, 45]), 4);
+        assert_eq!(
+            longest_sub_seq_map(&[36, 41, 56, 35, 91, 33, 34, 92, 43, 37, 42]),
             5
         );
     }
